225c605415a201bb2dfec15e7e38dcf8437448f2
ajeetjaiswal <ajeetjaiswal103@gmail.com>
2020-02-06 14:06:33 +0000

    add mongoDbAtlas

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ var commentRoutes      =  require("./routes/comments"),
 //===========================================//
 //M O N G O O S E   C O O N E C T I O N
 //============================================//
-mongoose.connect('mongodb://localhost:27017/yelp_camp_2', {useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost:27017/yelp_camp_2', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://yelpcamp:dragonkaku@cluster0-fkscf.mongodb.net/test?retryWrites=true&w=majority',{
+	useNewUrlParser: true,
+	useCreateIndex: true
+})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
